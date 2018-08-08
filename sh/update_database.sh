dd6e8cee5c09440351fd3d6835dd179757ab482d
CBONAMI2 <christian.bonami@vdab.be>
2018-08-08 14:34:06 +0200

    fix unfound db driver attempt 4

--- sh/update_database.sh
+++ sh/update_database.sh
@@ -2,6 +2,10 @@
 prop_replace 'nifi.registry.db.url'                         "${NIFI_REGISTRY_DB_URL:-jdbc:h2:./database/nifi-registry-primary;AUTOCOMMIT=OFF;DB_CLOSE_ON_EXIT=FALSE;LOCK_MODE=3;LOCK_TIMEOUT=25000;WRITE_DELAY=0;AUTO_SERVER=FALSE}"
 prop_replace 'nifi.registry.db.driver.class'                "${NIFI_REGISTRY_DB_CLASS:-org.h2.Driver}"
 prop_replace 'nifi.registry.db.driver.directory'            "${NIFI_REGISTRY_DB_DIR:-}"
+
+echo "postgres_pasword ${POSTGRES_PASSWORD}"
+echo "NIFI_REGISTRY_DB_PASS ${NIFI_REGISTRY_DB_PASS}"
+
 prop_replace 'nifi.registry.db.driver.username'             "${NIFI_REGISTRY_DB_USER:-nifireg}"
 prop_replace 'nifi.registry.db.driver.password'             "${NIFI_REGISTRY_DB_PASS:-nifireg}"
 prop_replace 'nifi.registry.db.driver.maxConnections'       "${NIFI_REGISTRY_DB_MAX_CONNS:-5}"
--- sh/update_flow_provider.sh
+++ sh/update_flow_provider.sh
@@ -1,12 +1,8 @@
 #!/bin/sh -e
 
-echo "update_flow_provider.sh 1"
-
 providers_file=${NIFI_REGISTRY_HOME}/conf/providers.xml
 property_xpath='/providers/flowPersistenceProvider'
 
-echo "update_flow_provider.sh $providers_file"
-cat $providers_file
 
 add_property() {
   property_name=$1
@@ -17,8 +13,6 @@ add_property() {
   fi
 }
 
-echo "update_flow_provider.sh 2"
-
 xmlstarlet ed --inplace -u "${property_xpath}/property[@name='Flow Storage Directory']" -v "${NIFI_REGISTRY_FLOW_STORAGE_DIR:-./flow_storage}" "${providers_file}"
 
 case ${NIFI_REGISTRY_FLOW_PROVIDER} in
@@ -26,14 +20,9 @@ case ${NIFI_REGISTRY_FLOW_PROVIDER} in
         xmlstarlet ed --inplace -u "${property_xpath}/class" -v "org.apache.nifi.registry.provider.flow.FileSystemFlowPersistenceProvider" "${providers_file}"
         ;;
     git)
-        echo "update_flow_provider.sh 3"
         xmlstarlet ed --inplace -u "${property_xpath}/class" -v "org.apache.nifi.registry.provider.flow.git.GitFlowPersistenceProvider" "${providers_file}"
-        echo "update_flow_provider.sh 4"
         add_property "Remote To Push"  "${NIFI_REGISTRY_GIT_REMOTE:-}"
-        echo "update_flow_provider.sh 5"
         add_property "Remote Access User"  "${NIFI_REGISTRY_GIT_USER:-}"
-        echo "update_flow_provider.sh 6"
         add_property "Remote Access Password"    "${NIFI_REGISTRY_GIT_PASSWORD:-}"
-        echo "update_flow_provider.sh end"
         ;;
 esac
