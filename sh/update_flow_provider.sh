cd6afedf139938a5393bd53131f52c165613f960
CBONAMI2 <christian.bonami@vdab.be>
2018-08-13 15:19:12 +0200

    Fixed flow definition (3)

--- sh/update_flow_provider.sh
+++ sh/update_flow_provider.sh
@@ -5,15 +5,21 @@ property_xpath='/providers/flowPersistenceProvider'
 
 
 add_property() {
+  echo "add_property $1 $2"
   property_name=$1
   property_value=$2
 
   if [ -n "${property_value}" ]; then
+    echo "add_property 1"
     xmlstarlet ed --inplace --subnode "/providers/flowPersistenceProvider" --type elem -n propertytemp -v "${property_value}" "${providers_file}"
+    echo "add_property 2"
     xmlstarlet ed --inplace --subnode "/providers/flowPersistenceProvider/propertytemp" --type attr -v "${property_name}" "${providers_file}"
+    echo "add_property 3"
     xmlstarlet ed --inplace --r "/providers/flowPersistenceProvider/propertytemp" -v "property" "${providers_file}"
   fi
 }
+
+echo $(xmlstarlet --version)
 
 xmlstarlet ed --inplace -u "${property_xpath}/property[@name='Flow Storage Directory']" -v "${NIFI_REGISTRY_FLOW_STORAGE_DIR:-./flow_storage}" "${providers_file}"
 
@@ -23,10 +29,7 @@ case ${NIFI_REGISTRY_FLOW_PROVIDER} in
         ;;
     git)
         xmlstarlet ed --inplace -u "${property_xpath}/class" -v "org.apache.nifi.registry.provider.flow.git.GitFlowPersistenceProvider" "${providers_file}"
-        #remove all property-s
-        xmlstarlet ed --inplace -d "/providers/flowPersistenceProvider/property" "${providers_file}"
         #add new properties
-        add_property "Flow Storage Directory"  "${NIFI_REGISTRY_FLOW_STORAGE_DIR:-}"
         add_property "Remote To Push"  "${NIFI_REGISTRY_GIT_REMOTE:-}"
         add_property "Remote Access User"  "${NIFI_REGISTRY_GIT_USER:-}"
         add_property "Remote Access Password"    "${NIFI_REGISTRY_GIT_PASSWORD:-}"
